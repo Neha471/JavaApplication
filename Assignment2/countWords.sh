bbc356695cb80f81bbab1aee3930ea2a464f32a9
Neha471 <nehashrivastava531@gmail.com>
2022-10-10 20:19:33 +0530

    Modified

--- Assignment2/countWords.sh
+++ Assignment2/countWords.sh
@@ -3,16 +3,16 @@
 
 if [ $# -ne 1 ];
 then
-		  echo "Usage: $0 filename";
-		  	    exit -1
+	echo "Usage: $0 filename";
+	exit -1
 fi
 
 filename=$1
 egrep -o "\b[[:alpha:]]+\b" $filename | \
 
-		awk '{ count[$0]++ }
+awk '{ count[$0]++ }
 END {printf("%-14s%s\n","Word","Count") ;
 PROCINFO["sorted_in"] = "@val_num_desc"
 for(ind in count)
 		{ printf("%-14s%d\n",ind,count[ind]); }
-	}'
+}'
