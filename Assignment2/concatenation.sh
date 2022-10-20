7065427208fa3cb72956afc49c571712d4395a2c
Neha471 <nehashrivastava531@gmail.com>
2022-10-20 14:27:44 +0530

    Modified concatenation.sh

--- Assignment2/concatenation.sh
+++ Assignment2/concatenation.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-javac Concate.java
-java Concate
+array=('ab' 'cd' 'ef')
+swap1() {
+	var=${array[1]}
+	array[1]=${array[0]}
+	array[0]=$var
+}
+swap2() {
+	var=${array[2]}
+	array[2]=${array[0]}
+	array[0]=$var
+}
+for  ((  i=0 ; i<6; i++ ))
+do
+	echo ${array[0]}${array[1]}${array[2]}
+	if [ `expr $i % 2` == 0 ]
+	then
+		swap1
+	else
+		swap2
+	fi
+done
